45c13119c970d266eac71876957a640a2c4aba64
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-04-12 00:09:38 +0200

    feat: save and unsave credentials

--- LearnFlow/app/src/main/java/com/example/learnflow/MainActivity.kt
+++ LearnFlow/app/src/main/java/com/example/learnflow/MainActivity.kt
@@ -14,7 +14,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cbWrapper: LinearLayout
     private lateinit var ciEmail: CustomInput
     private lateinit var ciPassword: CustomInput
+    private lateinit var cb: CheckBox
 
+    private val SP_CB_KEY = "cbCredentialsChecked"
     private val SP_EMAIL_KEY = "credentialEmail"
     private val SP_PASSWORD_KEY = "credentialPassword"
     private lateinit var sharedPreferences: SharedPreferences
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         cbWrapper = findViewById(R.id.cbWrapperMain)
         ciEmail = findViewById(R.id.ciEmail)
         ciPassword = findViewById(R.id.ciPassword)
+        cb = cbWrapper.children.elementAt(0) as CheckBox
 
         sharedPreferences = getSharedPreferences(
             getString(R.string.app_name),
@@ -35,17 +38,25 @@ class MainActivity : AppCompatActivity() {
         setOnClickListeners()
 
         ciEmail.et.setText(sharedPreferences.getString(SP_EMAIL_KEY, null))
+        ciPassword.et.setText(sharedPreferences.getString(SP_PASSWORD_KEY, null))
+        cb.isChecked = sharedPreferences.getBoolean(SP_CB_KEY, false)
     }
 
     private fun setOnClickListeners() {
-        val cb = cbWrapper.children.elementAt(0) as CheckBox
         val spEditor = sharedPreferences.edit()
         cbWrapper.setOnClickListener {
             cb.isChecked = !cb.isChecked
+            spEditor.putBoolean(SP_CB_KEY, cb.isChecked)
             if (cb.isChecked) {
                 spEditor.putString(SP_EMAIL_KEY, ciEmail.et.text.toString())
                 spEditor.putString(SP_PASSWORD_KEY, ciPassword.et.text.toString())
                 spEditor.apply()
+            } else {
+                spEditor
+                    .remove(SP_EMAIL_KEY)
+                    .remove(SP_PASSWORD_KEY)
+                    .remove(SP_CB_KEY)
+                    .apply()
             }
         }
     }
