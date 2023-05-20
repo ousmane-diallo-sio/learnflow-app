ca951d6605404738f334480f1be25922023560ae
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-05-20 19:31:56 +0200

    chore: setting up env file

---
+++ LearnFlow/app/src/main/java/com/example/learnflow/utils/EnvUtils.kt
@@ -0,0 +1,15 @@
+package com.example.learnflow.utils
+
+import io.github.cdimascio.dotenv.dotenv
+
+object EnvUtils {
+
+    private val dotenv = dotenv {
+        directory = "/assets"
+        filename = "env" // instead of '.env', use 'env'
+    }
+
+    private const val placeholder = "value-not-found"
+
+    val API_BASE_URL = dotenv["API_BASE_URL"] ?: placeholder
+}
--- LearnFlow/app/src/main/java/com/example/learnflow/webservices/Api.kt
+++ LearnFlow/app/src/main/java/com/example/learnflow/webservices/Api.kt
@@ -2,17 +2,22 @@ package com.example.learnflow.webservices
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.example.learnflow.BuildConfig
 import com.example.learnflow.HomeActivity
 import com.example.learnflow.MainActivity
 import com.example.learnflow.model.User
+import com.example.learnflow.utils.EnvUtils
+import io.github.cdimascio.dotenv.dotenv
 
 object Api {
     var currentUser: User? = null
 
     fun login(context: Context, email: String, password: String) {
         ContextCompat.startActivity(context, Intent(context, HomeActivity::class.java), null)
+        Log.d("Components", EnvUtils.API_BASE_URL)
     }
 
     fun register(user: User) {
